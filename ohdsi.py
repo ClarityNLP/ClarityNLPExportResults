7eda3f9b8603a23729da5e09d8bf39e95c48a6f1
cjamadagni <chirag.jamadagni@gmail.com>
2018-09-05 12:40:21 -0400

    adding delete stuff

--- ohdsi.py
+++ ohdsi.py
@@ -67,6 +67,7 @@ class ExportOhdsi:
             conn.commit()
         except:
             return Response('{"message":"Cant write data to OMOP database"}', status=500, mimetype='application/json')
+            self.deleteExportedResults(cursor, conn, primary_key, omop_domain)
 
         conn.close()
 
@@ -115,10 +116,42 @@ class ExportOhdsi:
                 report_date = datetime.strptime(entry['report_date'][0:entry['report_date'].find('T')], '%Y-%m-%d')
                 source_value = "Job=" + str(entry['job_id']) + "|Pipeline=" + str(entry['pipeline_id'])
                 query = "INSERT INTO " + util.ohdsi_schema + ".condition_occurrence(condition_occurrence_id, person_id, condition_concept_id, condition_start_date, condition_type_concept_id, condition_source_value) VALUES (%s, %s, %s, %s, %s, %s)"
-                cursor.execute(query, (primary_key, entry['subject'], concept_id, report_date, domain_map[omop_domain], source_value))
+                try:
+                    cursor.execute(query, (primary_key, entry['subject'], concept_id, report_date, domain_map[omop_domain], source_value))
+                except:
+                    continue
+
             elif omop_domain == 'Measurement':
                 report_date = datetime.strptime(entry['report_date'][0:entry['report_date'].find('T')], '%Y-%m-%d')
                 query = "INSERT INTO " + util.ohdsi_schema + ".measurement(measurement_id, person_id, measurement_concept_id, measurement_date, measurement_type_concept_id, value_as_number) VALUES (%s, %s, %s, %s, %s, %s)"
-                cursor.execute(query, (primary_key, entry['subject'], concept_id, report_date, domain_map[omop_domain], entry['value']))
+                try:
+                    cursor.execute(query, (primary_key, entry['subject'], concept_id, report_date, domain_map[omop_domain], entry['value']))
+                except:
+                    continue
+
+            elif omop_domain == 'Observation':
+                report_date = datetime.strptime(entry['report_date'][0:entry['report_date'].find('T')], '%Y-%m-%d')
+                query = "INSERT INTO " + util.ohdsi_schema + ".observation(observation_id, person_id, observation_concept_id, observation_date, observation_type_concept_id, value_as_number) VALUES (%s, %s, %s, %s, %s, %s)"
+                try:
+                    cursor.execute(query, (primary_key, entry['subject'], concept_id, report_date, domain_map[omop_domain], entry['value']))
+                except:
+                    continue
 
             break # TODO: remove this break
+
+    """
+    Function to delete recently added results
+    """
+    def deleteExportedResults(self, cursor, conn, primary_key, omop_domain):
+        if omop_domain == 'Condition':
+            query = "DELETE FROM " + util.ohdsi_schema + ".condition_occurrence WHERE condition_occurrence_id=%s"
+        elif omop_domain == 'Measurement':
+            query = "DELETE FROM " + util.ohdsi_schema + ".measurement WHERE measurement_id=%s"
+        elif omop_domain == 'Observation':
+            query = "DELETE FROM " + util.ohdsi_schema + ".observation WHERE observation_id=%s"
+
+        cursor.execute(query, (primary_key, ))
+        conn.commit()
+
+
+        cursor.execute()
